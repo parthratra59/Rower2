396e6fd3472c8f679b030bc1b667be573b1af91d
Parth Ratra <ratraparth59@gmail.com>
2023-03-21 14:39:40 +0530

    navbar has been fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 // import styled from "styled-components";
-// import { IoIosAddCircleOutline } from "react-icons/io";
-// import {CgMenu,CgCloseO} from "react-icons/cg"
 import GlobalStyle from "../GlobalStyle";
+import {BsPersonCircle} from "react-icons/bs"
+import {GrAddCircle} from "react-icons/gr"
 const Navbar=()=>{ 
     <GlobalStyle/>
     return(
@@ -13,21 +13,23 @@ const Navbar=()=>{
                     
                         <ul className="leftchild">
                             <li>
-                                <NavLink to='/'>Home</NavLink>
+                                <NavLink to='/' style={{textDecoration:'none'}} >Home</NavLink>
                             </li>
                             <li>
-                            <NavLink to='/'>Publish to ride</NavLink>
+                            <NavLink to='/' style={{textDecoration:'none'}} className="navlink"><GrAddCircle className="add" size={18}/>Publish a ride</NavLink>
                             </li>
                         </ul>
                   
                     
                         <ul className="rightchild">
+                            <div className="sub-child">
                             <li>
-                                <NavLink to='/'>Login</NavLink>
+                                <NavLink className='signbtn' to='/' style={{textDecoration:'none'}}><BsPersonCircle className="design"  size={25} color='black'/>Sign in</NavLink>
                             </li>
-                            <li>
-                                <NavLink to='/'>Signin</NavLink>
-                            </li>
+                            </div>
+                            {/* <li>
+                                <NavLink to='/' style={{textDecoration:'none'}}>Signin</NavLink>
+                            </li> */}
                         </ul>
                   
 
